ec8f707380460b5812db70c533bc3137006b918e
CaioCDJ <caiodjesus1@hotmail.com>
2022-06-15 17:02:56 -0300

    Navigation with react-router

---
+++ frontend/src/common/template/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default props =>(
+
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022
+            <a href="http://cod3r.com.br" target='_blanck'>Cod3r</a>
+        </strong>
+    </footer>
+);
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 
 import Header from '../common/template/Header';
 import SideBar from '../common/template/sideBar';
+import Footer from '../common/template/Footer';
+import Routes from './routes';
 
 export default props => (
 
     <div className="wrapper">
         <Header></Header>
         <SideBar/>
+        <div className="content-wrapper">
+            <Routes/>
+        </div>
+        <Footer/>
     </div>
 );
