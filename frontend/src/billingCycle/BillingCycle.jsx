2be495114f0e5b8c5e2e6b6e98e1197d8ec54a5e
CaioCDJ <caiodjesus1@hotmail.com>
2022-06-20 15:20:20 -0300

    Tab Header

--- frontend/src/billingCycle/BillingCycle.jsx
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React,{Component} from "react";
 
-export default props=>(
+import Content from '../common/template/content.jsx';
+import ContentHeader from '../common/template/contentHeader.jsx';
 
-    <div>
-        <h1>Ciclo de pagamentos</h1>
-    </div>
-);
+import Tabs from '../common/tab/tabs.jsx';
+import TabHeader from '../common/tab/tabHeader.jsx';
+import TabsHeader from '../common/tab/tabsHeader.jsx';
+import TabsContent from '../common/tab/tabsContent.jsx';
+
+class BillingCycle extends Component{
+    
+    render(){
+
+        return(
+            <div>
+                <ContentHeader title="Ciclos de pagamentos" 
+                               small="cadastro"/>
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='listar' icon='bars' target='tagList'/>
+                            <TabHeader label='Incluir' icon='plus' target='tagCreate'/>
+                            <TabHeader label='Alterar' icon='pencil' target='tagUpdate'/>
+                            <TabHeader label='Excluir' icon='trash-o' target='tagDelete'/>
+                        </TabsHeader>
+                        <TabsContent></TabsContent>
+                    </Tabs>    
+                </Content>
+            </div>
+        )
+
+    }
+}
+
+export default BillingCycle;
